aa9d0d9f28fc01e69f404ed07024d7eafa480ea3
张单旸晖 <236294386@qq.com>
2017-11-01 12:35:43 +0800

    implement start button: page can scroll when clicked

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -1,4 +1,16 @@
-$(window).on("load resize",function(){
-    var h=window.innerHeight||document.body.clientHeight||document.documentElement.clientHeight;
-    $(".bg-attachment1 .bg-attachment-hidden").css("height",h);
+//首屏自适应高度
+var $window = $(window);
+$window.on("load resize", function () {
+    var h = window.innerHeight || document.body.clientHeight || document.documentElement.clientHeight;
+    $(".bg-attachment1 .bg-attachment-hidden").css("height", h);
+});
+
+$window.on("load",function () {
+    var scrll_to_start = function () {
+        $('html,body').animate({scrollTop: $('#start').offset().top}, 800);
+    };
+    //start按钮滚动到指定位置
+    $("#img_start_button").click(scrll_to_start);
+    $("#txt_start_button").click(scrll_to_start);
+    $("#img_down_arrow").click(scrll_to_start);
 });
