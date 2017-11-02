741217d46180ec21483dee53830ebb1e5859f538
张单旸晖 <236294386@qq.com>
2017-11-02 14:00:25 +0800

    add start button hover effect

--- src/main/webapp/index.js
+++ src/main/webapp/index.js
@@ -9,7 +9,24 @@ $(document).ready(function () {
         $('html,body').animate({scrollTop: $('#start').offset().top}, 800);
     };
     //start按钮滚动到指定位置
-    $("#img_start_button").click(scrll_to_start);
-    $("#txt_start_button").click(scrll_to_start);
+    var $img_start_button = $("#img_start_button");
+    var $txt_start_button = $("#txt_start_button");
+
+    $img_start_button.click(scrll_to_start);
+    $txt_start_button.click(scrll_to_start);
     $("#img_down_arrow").click(scrll_to_start);
+
+
+    //start button hover
+    var img_start_button_hover = function () {
+        $img_start_button.css("background","url(imgs/start_button_hover.png) no-repeat center center");
+        $img_start_button.css("background-size","95% 95%");
+    };
+
+    var img_start_button_hover_out = function () {
+        $img_start_button.css("background","url(imgs/start_button.png) no-repeat center center");
+        $img_start_button.css("background-size","95% 95%");
+    };
+    $img_start_button.hover(img_start_button_hover,img_start_button_hover_out);
+    $txt_start_button.hover(img_start_button_hover,img_start_button_hover_out);
 });
