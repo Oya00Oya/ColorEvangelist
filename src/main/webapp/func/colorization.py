4bf20800c1bc9ff025283e313b600ded97cd68c3
orashi <550404990@qq.com>
2018-03-01 00:15:42 +0800

    :tada: chainer workflow

--- src/main/webapp/func/colorization.py
+++ src/main/webapp/func/colorization.py
@@ -1,19 +1,16 @@
 import logging
-import torch.utils.data
-import torch.nn as nn
-import torchvision.transforms as transforms
-from torch.autograd import Variable
-import torch.nn.functional as F
-from PIL import Image
+import chainer
+import chainer.functions as F
+from chainer import Variable
+import chainercv
+from PIL import Image, ImageEnhance
 import cv2
 import sys
 import os
 import json
 import numpy as np
 import socket
-from models.iv_model import def_netG
-
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+from models.model import NetG
 
 logger = logging.getLogger("logger")
 logger.setLevel(logging.DEBUG)
@@ -25,7 +22,8 @@ logger.addHandler(ch)
 
 
 def denoise(img):
-    return Image.fromarray(cv2.fastNlMeansDenoising(np.asarray(img), None, 10, 7, 21))
+    return ImageEnhance.Contrast(img).enhance(
+        1.0)  # Image.fromarray(cv2.fastNlMeansDenoising(np.asarray(img), None, 10, 7, 21))).enhance(1.5)
 
 
 web = socket.socket()
@@ -35,19 +33,14 @@ web.listen(5)
 desire_min = 512.0
 args = sys.argv
 
-netG = torch.nn.DataParallel(def_netG(ngf=64))
-netG.load_state_dict(torch.load('../webapps/ROOT/func/netG_epoch_only_YanACIV_check1.pth'))
-netG.cuda().eval()
-to_tensor, to_pil = transforms.ToTensor(), transforms.ToPILImage()
-ts = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+netG = NetG(ngf=64)
+to_pil = lambda x: Image.fromarray(((x.array / 2 + 0.5) * 255).transpose(1, 2, 0))
+ts = lambda x: F.expand_dims(Variable((np.asarray(x).astype('float') / 255) * 2 - 1), 0)
 
-ts2 = transforms.Compose([
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
+ts2 = lambda x: x * 2 - 1
 
 print("model loaded")
-
+chainercv.transforms.resize
 while True:
     try:
         conn, addr = web.accept()
@@ -68,30 +61,30 @@ while True:
         # retrieve down color map
         colormap = Image.new('RGBA', desire_size, (255, 255, 255))
         colormap.paste(back, (0, 0, desire_size[0], desire_size[1]), back)
-        colormap = F.max_pool2d(
-            Variable((to_tensor(np.array(colormap.convert('RGB')))).cuda().unsqueeze(0)) * -1, 4, 4) * -1
+        colormap = F.max_pooling_2d(
+            F.expand_dims(Variable(np.array(colormap.convert('RGB')).astype('float')).transpose(2, 0, 1), 0) * -1, 4,
+            4) * -1
 
         # retrieve down valid mask
-        valid_mask = F.max_pool2d(Variable(
-            (torch.FloatTensor(np.array(back)[:, :, 3].astype('float'))).gt(254).float().cuda().unsqueeze(0).unsqueeze(
-                0)), 4, 4)
+        valid_mask = F.max_pooling_2d(
+            F.expand_dims(F.expand_dims(Variable((np.array(back)[:, :, 3].astype('float') > 254).astype('float')), 0),
+                          0),
+            4, 4)
 
-        sketch, colormap = Variable(ts(sketch)).unsqueeze(0).cuda(), Variable(ts2(colormap.squeeze().data).unsqueeze(0))
+        sketch, colormap = F.expand_dims(ts(sketch), 0), ts2(colormap)
 
         h, w = colormap.shape[2] // 2, colormap.shape[3] // 2
         mask = Variable(
-            torch.FloatTensor(([1, 0] * h + [0, 1] * h) * w).view(colormap.shape[2], colormap.shape[3]).cuda())
-        mask = mask * valid_mask
-        noise = torch.Tensor(1, 64, 1, 1).normal_(0, 1).cuda()
+            np.array(([1, 0] * h + [0, 1] * h) * w).reshape(colormap.shape[2], colormap.shape[3]).astype('float'))
+        mask = F.expand_dims(F.expand_dims(mask, 0), 0) * valid_mask
 
-        hint = torch.cat((colormap * mask, mask), 1)
+        hint = F.concat((colormap * F.broadcast_to(mask, (1, 3, colormap.shape[2], colormap.shape[3])), mask), 1)
 
-        with torch.no_grad():
-            out = netG(sketch,
-                       hint,
-                       Variable(noise),
-                       ).data
-        to_pil(out.mul(0.5).add(0.5).squeeze().cpu()).resize(ori_size, Image.BICUBIC).save(msg["out"])
+        with chainer.using_config('train', False):
+            out = netG(F.cast(sketch, np.float32),
+                       F.cast(hint, np.float32),
+                       )
+        to_pil(F.squeeze(out * 0.5 + 0.5)).resize(ori_size, Image.BICUBIC).save(msg["out"])
 
         sent = conn.send('Success\r\n'.encode('utf-8'))
         print("sent {}".format(sent))
