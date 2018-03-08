f53c31d5b7c9ea7d4a2a6c00152d460bef9ecd2b
orashi <550404990@qq.com>
2018-03-08 15:47:48 +0800

    sketch algo update

--- src/main/webapp/func/sketch.py
+++ src/main/webapp/func/sketch.py
@@ -6,23 +6,53 @@ import numpy as np
 import os
 import os.path
 import sys
+from PIL import Image
 
 IMG_EXTENSIONS = [
     '.jpg', '.JPG', '.jpeg', '.JPEG',
     '.png', '.PNG', '.ppm', '.PPM', '.bmp', '.BMP',
 ]
 
-
 args = sys.argv
 
-def demoDoG(img, sig=0.55, tau=0.95, phi=10000):
-    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+def small_component_removal(img):
+    """
+    remove small chokes
+    :param imgs: 1 channel opencv np array 0-255
+    :return: 1 channel opencv np array 0-255
+    """
+    thresh = 127
+    im_bw = cv2.threshold(img, thresh, 255, cv2.THRESH_BINARY)[1]
+    im_bw = im_bw * -1 + 255
+    # find connected black components
+    nb_components, output, stats, centroids = cv2.connectedComponentsWithStats(im_bw.astype(np.uint8), connectivity=8)
+
+    sizes = stats[1:, -1]
+    nb_components = nb_components - 1
+
+    min_size = 10
+
+    # answer image
+    img2 = np.zeros(output.shape)
+    for i in range(0, nb_components):
+        if sizes[i] >= min_size:
+            img2[output == i + 1] = 255
+    return img2.astype(np.float32) * -1 + 255
+
+
+def XDoG_S(img, sig):
+    gray = np.array(img.convert('L')) / 255
     xdog_params = {
-        'kappa': 4.5, 'sigma': sig, 'tau': tau, 'phi': phi, # sig: continuity  tau:agile threshold  phi: step func
+        'kappa': 1.0001, 'sigma': sig, 'tau': 0.999997, 'phi': 1e20,
         'edgeType': lime.NPR_EDGE_XDOG
     }
-    xdog = lime.edgeDoG(gray, xdog_params)
-    return xdog
+    edge = small_component_removal(lime.edgeDoG(gray.astype(np.float32), xdog_params) * 255)
+    return Image.fromarray(edge, mode='I').convert('RGB')
+
+
+def resize_by(img, side_min):
+    return img.resize((int(img.size[0] / min(img.size) * side_min), int(img.size[1] / min(img.size) * side_min)),
+                      Image.BICUBIC)
 
 
 def is_image_file(filename):
@@ -30,10 +60,11 @@ def is_image_file(filename):
 
 
 def default_loader(path):  # fixed
-    return cv2.imread(path, cv2.IMREAD_COLOR)
+    return Image.open(path).convert('RGB')
 
 
-img = cv2.imread(args[1], cv2.IMREAD_COLOR)
-img = demoDoG((img / 255.0).astype(np.float32), sig=0.5) * 255.0
-cv2.imwrite(args[2], img)
+image = default_loader(args[1])
+image = resize_by(image, 750)
+edge = XDoG_S(image, sig=0.7)
+edge.save(args[2])
 print('Success')
